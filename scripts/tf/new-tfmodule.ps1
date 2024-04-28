073f9372b8fbdb55627233c7b1190758ef1dc992
Drew Burt <daredevelops@gmail.com>
2024-04-28 17:53:31 -0400

    changed the tf module creation script from a basic module to a full module.

--- scripts/tf/new-tfmodule.ps1
+++ scripts/tf/new-tfmodule.ps1
@@ -1,29 +1,33 @@
 <#
 .SYNOPSIS
- Create a base terraform module
+Creates the basic file structure for a Terraform solution.
 .DESCRIPTION
+This tool create the base module set for an Azure Soluton Terafrom module. You have the option to create amodules
+subdirectory to host additional external modules.
 #>
-
-
-$moduleName = Read-Host -Prompt "Please enter a module name"
-
-# Create project folder
-$p = New-Item -Path . -Name $moduleName -ItemType Directory
-Set-Location -Path $p
-
-
-# Create the base files
-
+$modpath = get-location
+ # New-Item -Path . -Name readme.md -ItemType File
+New-Item -Path . -Name terraform.tf -ItemType File
 New-Item -Path . -Name main.tf -ItemType File
-New-Item -Path . -Name backend.tf -ItemType File
-New-Item -Path . -Name variable.tf -ItemType File
+New-Item -Path . -Name variables.tf -ItemType File
 New-Item -Path . -Name outputs.tf -ItemType File
-New-Item -Path . -Name README.md -ItemType File -
 
+$path = $PSScriptroot
+set-location $path
 
-# Create nested modules directory
-$createDir = Read-Host -Prompt "Do you require a nested sub-module Y or N?"
+$tfVersion = Get-Content -raw -path ./json/versions.json
+New-Item -Path $modpath  -Name provdider.tf -ItemType File -value $tfVersion
 
-if ($createDir -eq 'Y' ) {
+$tfbackend =  Get-Content -raw -path ./json/backend.json
+New-Item -Path $modpath -Name backend.tf -ItemType file -value $tfbackend
+
+$tfReadme = "TODO: Update the Backend.tf file to your backend state file
+Update provider version to static version currently set to auto update. Change the container name and key the 
+resource group and storage account are set"
+New-Item -Path $modpath -Name README.md -ItemType file -Value $tfReadme
+set-location $modpath
+
+$createModuleDirectory = Read-Host -Prompt "Do you require a modules folder Y or N?"
+if ($createModuleDirectory -eq 'Y') {
     New-Item -Path . -Name modules -ItemType Directory
 }
